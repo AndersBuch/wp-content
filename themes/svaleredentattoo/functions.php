ca7fbf742f0bfa0e0255f82b245353d07ce68ed8
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2023-11-20 10:08:37 +0100

    added files

--- themes/svaleredentattoo/functions.php
+++ themes/svaleredentattoo/functions.php
@@ -1,6 +1,25 @@
 <?php
-register_nav_menus(array(
-'main_menu' => 'Main Menu',
-'footer_menu' => 'Footer Menu'
-));
-?>
+
+// Load Stylesheets
+function load_css()
+{
+
+		wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all' );
+		wp_enqueue_style('main');
+	
+}
+add_action('wp_enqueue_scripts','load_css');
+
+// Theme Options
+add_theme_support('menus');
+
+// Menus
+register_nav_menus(
+
+    array(
+
+        'top-menu' => 'Top Menu Location',
+        'footer-menu' => 'Footer Menu Location',
+    )
+
+);
